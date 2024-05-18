4da44ffce89f0472bcee2945be1df08ace3e010d
DenisHorvat1 <denishorvat2002@gmail.com>
2024-05-18 15:53:37 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HotelListComponent } from './hotel-list/hotel-list.component';
+import { HotelDetailComponent } from './hotel-detail/hotel-detail.component';
+import { RoomBookingComponent } from './room-booking/room-booking.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HotelListComponent,
+    HotelDetailComponent,
+    RoomBookingComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
